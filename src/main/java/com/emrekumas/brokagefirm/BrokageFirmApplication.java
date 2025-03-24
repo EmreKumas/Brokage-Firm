58e90316cdcbdbcfad1fe3e6d84c7cb90edc2427
emre.kumas <emre-kumas@outlook.com>
2025-03-24 15:26:20 +0300

    Bend initial code to my liking

--- src/main/java/com/emrekumas/brokagefirm/BrokageFirmApplication.java
+++ src/main/java/com/emrekumas/brokagefirm/BrokageFirmApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrokageFirmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BrokageFirmApplication.class, args);
 	}
-
 }
